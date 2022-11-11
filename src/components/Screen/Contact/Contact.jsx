41b4c91824cabf89a1847eda32ffed7dc58ced64
FrankoSaf <franko.safradin13@gmail.com>
2022-11-11 12:45:48 +0100

    adding contact form

--- src/components/Screen/Contact/Contact.jsx
+++ src/components/Screen/Contact/Contact.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import '../../../UI/Screen/Home.scss';
+import React from "react";
+import "../../../UI/Screen/Contact.scss";
 const Contact = () => {
   return (
-    <div className='home'>
-      <h1>Contact</h1>{' '}
+    <div className="contact-form_container">
+      <form action="" className="form">
+        <div>
+          <label htmlFor="">Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Your message here</label>
+          <textarea name="message"></textarea>
+        </div>
+        <button>Send something</button>
+      </form>
     </div>
   );
 };
