faae972a8a8e07e6d462acc134210b97385766c1
FrankoSaf <franko.safradin13@gmail.com>
2022-11-16 15:32:33 +0100

    something something

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -1,29 +1,30 @@
-import React, { useContext } from 'react';
-import { ScreenContext } from '../../store/ScreenContext';
-import About from './About/About';
-import Contact from './Contact/Contact';
-import Home from './Home';
-import Projects from './Projects/Projects';
-
+import React, { useContext } from "react";
+import { ScreenContext } from "../../store/ScreenContext";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Home from "./Home";
+import Projects from "./Projects/Projects";
+import "../../UI/Stars.scss";
 const Screen = ({ className, style }) => {
   const { screen, fullScreen, setFullScreen } = useContext(ScreenContext);
 
   return (
     <div className={className} style={style}>
+      <div className="stars"></div>
       {fullScreen && (
         <p>
-          Press{' '}
-          <span onClick={() => setFullScreen(false)} style={{ color: 'red' }}>
+          Press{" "}
+          <span onClick={() => setFullScreen(false)} style={{ color: "red" }}>
             here
-          </span>{' '}
+          </span>{" "}
           or hit ESC to exit full screen
         </p>
       )}
-      {screen === 'home' ? (
+      {screen === "home" ? (
         <Home />
-      ) : screen === 'contact' ? (
+      ) : screen === "contact" ? (
         <Contact />
-      ) : screen === 'about' ? (
+      ) : screen === "about" ? (
         <About />
       ) : (
         <Projects />
