dec1ecb484a1e4f9d2d988ec222cca87a0a4b284
FrankoSaf <franko.safradin@gmail.com>
2022-11-16 14:15:46 +0100

    footer added

--- src/components/Screen/Projects/Projects.jsx
+++ src/components/Screen/Projects/Projects.jsx
@@ -1,58 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import '../../../UI/Screen/Projects.scss';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import Cards from '../../UI/Cards';
+import { projects } from '../../../assets/projects/projects';
+import { ScreenContext } from '../../../store/ScreenContext';
 const Projects = () => {
+  const { joystick } = useContext(ScreenContext);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= projects.length) {
+      newIndex = projects.length - 1;
+    }
+    setActiveIndex(newIndex);
+  };
+
+  useEffect(() => {
+    if (activeIndex >= 0 && activeIndex <= projects.length - 1) {
+      if (joystick.direction === 'left') {
+        setActiveIndex(activeIndex - 1);
+      } else if (joystick.direction === 'right') {
+        setActiveIndex(activeIndex + 1);
+      }
+    }
+
+    if (activeIndex < 0) setActiveIndex(0);
+    if (activeIndex > projects.length - 1) setActiveIndex(projects.length - 1);
+  }, [joystick]);
   return (
     <div className='project_container'>
-      <div className='arrow arrow_right'>
-        <FaArrowRight />
-      </div>
-      <div className='arrow arrow_left'>
-        {' '}
-        <FaArrowLeft />
-      </div>
-      <div className='card'>
-        <div className='card_image'>
-          <img
-            src='https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg'
-            alt=''
-          />
+      {activeIndex < projects.length - 1 && (
+        <div
+          className='arrow arrow_right'
+          onClick={() => updateIndex(activeIndex + 1)}
+        >
+          <FaArrowRight />
         </div>
-        <div className='card_description'>
-          <h2 className='card_description-header'>Project name</h2>
-          <p className='card-description-about'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea corporis
-            magni error eum laboriosam tempora adipisci repellat quasi
-            blanditiis totam corrupti esse officiis optio hic omnis nihil, rem
-            aut aliquam.
-          </p>
+      )}
+      {activeIndex > 0 && (
+        <div
+          className='arrow arrow_left'
+          onClick={() => updateIndex(activeIndex - 1)}
+        >
+          <FaArrowLeft />
         </div>
-        <div className='card_links'>
-          <a href='https://github.com/'>Github</a>
-          <a href='https://render.com/'>Live</a>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card_image'>
-          <img
-            src='https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg'
-            alt=''
-          />
-        </div>
-        <div className='card_description'>
-          <h2 className='card_description-header'>Project name</h2>
-          <p className='card-description-about'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea corporis
-            magni error eum laboriosam tempora adipisci repellat quasi
-            blanditiis totam corrupti esse officiis optio hic omnis nihil, rem
-            aut aliquam.
-          </p>
-        </div>
-        <div className='card_links'>
-          <a href='https://github.com/'>Github</a>
-          <a href='https://render.com/'>Live</a>
-        </div>
-      </div>
+      )}
+      {projects.map((project, index) => (
+        <Cards
+          name={project.name}
+          image={project.image}
+          github={project.links[0]}
+          render={project.links[1]}
+          description={project.description}
+          style={{
+            opacity: `${activeIndex !== index ? 0 : 1}`,
+            transition: '2s all',
+            transform: `translateX(-${activeIndex * 100}%)`,
+          }}
+        />
+      ))}
     </div>
   );
 };
