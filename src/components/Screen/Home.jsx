ebc616949f1f1e7f541ae0d94699e416cc8f818a
FrankoSaf <franko.safradin13@gmail.com>
2022-11-18 15:31:19 +0100

    styling

--- src/components/Screen/Home.jsx
+++ src/components/Screen/Home.jsx
@@ -1,23 +1,22 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { ScreenContext } from '../../store/ScreenContext';
-import '../../UI/Screen/Home.scss';
+import React, { useContext, useState, useEffect } from "react";
+import { ScreenContext } from "../../store/ScreenContext";
+import "../../UI/Screen/Home.scss";
 const Home = () => {
-  const links = ['ABOUT', 'PROJECTS', 'CONTACT'];
+  const links = ["ABOUT", "PROJECTS", "CONTACT"];
   const { screen, setScreen, fullScreen, joystick } = useContext(ScreenContext);
   const [selectedLink, setSelectedLink] = useState(0);
 
   useEffect(() => {
-    if (joystick.direction === 'up') {
+    if (joystick.direction === "up") {
       setSelectedLink(selectedLink - 1);
       if (selectedLink <= 0) setSelectedLink(0);
-    } else if (joystick.direction === 'down') {
+    } else if (joystick.direction === "down") {
       setSelectedLink(selectedLink + 1);
       if (selectedLink >= links.length - 1) setSelectedLink(links.length - 1);
     }
-    if (screen === 'home') {
-      window.addEventListener('keyup', (e) => {
-
-        return e.key.toLowerCase() === 'enter'
+    if (screen === "home") {
+      window.addEventListener("keyup", (e) => {
+        return e.key.toLowerCase() === "enter"
           ? setScreen(links[selectedLink].toLowerCase())
           : null;
       });
@@ -25,25 +24,28 @@ const Home = () => {
   }, [joystick]);
 
   return (
-    <div className='home'>
+    <div className="home">
       <h1>
         <span>Welcome to my</span> <span>portfolio</span>
       </h1>
       <ul>
         {links.map((item, index) => {
           return (
-            <li>
+            <li
+              style={{ cursor: "pointer" }}
+              onClick={() => setScreen(item.toLowerCase())}
+            >
               {item}
               {index === selectedLink && !fullScreen && (
                 <>
-                  <span className='border t tb'></span>
-                  <span className='border tr trbl'></span>
-                  <span className='border r rl'></span>
-                  <span className='border br tlbr'></span>
-                  <span className='border b tb'></span>
-                  <span className='border bl trbl'></span>
-                  <span className='border l rl'></span>
-                  <span className='border tl tlbr'></span>
+                  <span className="border t tb"></span>
+                  <span className="border tr trbl"></span>
+                  <span className="border r rl"></span>
+                  <span className="border br tlbr"></span>
+                  <span className="border b tb"></span>
+                  <span className="border bl trbl"></span>
+                  <span className="border l rl"></span>
+                  <span className="border tl tlbr"></span>
                 </>
               )}
             </li>
